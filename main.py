f324a0c3a411dc14f7955246afa6d10bcd6c92f7
mpeschek <michaelpeschek@icloud.com>
2025-12-07 21:24:35 +0100

    feat: correlation matrix | random forest training

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import time
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 
 def main():
@@ -8,8 +10,15 @@ def main():
     
     X_train, X_test, y_train, y_test = split_data(df)
 
+    # correlation_matrix = get_correlation(X_train, y_train)
+
+    trained_model = random_forest(X_train, y_train)
+
 
 def get_data():
+    '''
+    import and clean the train data
+    '''
     df = pd.read_csv("data/train.csv", index_col=0)
     df = pd.get_dummies(
         df,
@@ -46,6 +55,37 @@ def split_data(df):
     )
 
     return X_train, X_test, y_train, y_test
+
+
+def get_correlation(X, y):
+    '''
+    Get correlation matrix only from the training data
+    !!! Not include the test data !!!
+    '''
+    analysis_df = X.copy()
+    analysis_df['diagnosed_diabetes'] = y
+    
+    corr = analysis_df.corr(method="pearson")
+    print(corr['diagnosed_diabetes'].sort_values())
+
+    return corr
+
+
+def random_forest(X, y):
+    '''
+    random forest model
+    '''
+    rf_model = RandomForestClassifier(n_estimators=200, random_state=42, n_jobs=-1)
+
+    print("random forest model getting trained\n...")
+    time_start = time.perf_counter()
+
+    rf_model.fit(X, y)
+
+    time_end = time.perf_counter()
+    print(f"training finished | training time: {(time_end - time_start):.2f} s")
+
+    return rf_model
 
 
 if __name__ == "__main__":
