af5754454c2bd0c0e924de0f2664c09156744b73
mpeschek <michaelpeschek@icloud.com>
2025-12-08 20:02:44 +0100

    feat: blending different model (logistic_regression and xgboost)

---
+++ blend_predictions.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def main():
+    lr_submission = pd.read_csv("submissions/2025_12_08_diabetes_prediction_logistic_regression.csv")
+    xgb_submission = pd.read_csv("submissions/2025_12_08_diabetes_prediction_xgboost.csv")
+
+    blend_pred = pd.DataFrame({
+        'id': xgb_submission['id'],
+        'diagnosed_diabetes': (0.7 * xgb_submission['diagnosed_diabetes']) + (0.3 * lr_submission['diagnosed_diabetes'])
+    })
+
+    blend_pred.to_csv("submissions/2025_12_08_diabetes_prediction_blend_lr_xgb.csv", index=False)
+
+if __name__ == "__main__":
+    main()
