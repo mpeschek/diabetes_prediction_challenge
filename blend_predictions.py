fd5f6eb38b83c5cb3787d22e9f89426b18fcefe2
mpeschek <michaelpeschek@icloud.com>
2025-12-09 22:56:31 +0100

    feat: new merging (worse result) | xgb distracted by logistic regression

--- blend_predictions.py
+++ blend_predictions.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 def main():
     lr_submission = pd.read_csv("submissions/2025_12_08_diabetes_prediction_logistic_regression.csv")
-    xgb_submission = pd.read_csv("submissions/2025_12_08_diabetes_prediction_xgboost.csv")
+    xgb_submission = pd.read_csv("submissions/2025_12_09_xgb_kfold_0.7231.csv")
 
     blend_pred = pd.DataFrame({
         'id': xgb_submission['id'],
-        'diagnosed_diabetes': (0.75 * xgb_submission['diagnosed_diabetes']) + (0.25 * lr_submission['diagnosed_diabetes'])
+        'diagnosed_diabetes': (0.7 * xgb_submission['diagnosed_diabetes']) + (0.3 * lr_submission['diagnosed_diabetes'])
     })
 
-    blend_pred.to_csv("submissions/2025_12_08_diabetes_prediction_blend_lr_xgb.csv", index=False)
+    blend_pred.to_csv("submissions/2025_12_09_diabetes_prediction_blend_lr_xgb.csv", index=False)
 
 if __name__ == "__main__":
     main()
